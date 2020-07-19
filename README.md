3d3e4a97a6a125483092ca978b7ff27f50b19258
Ryokohbato <ryokohbato@gmail.com>
2020-07-19 23:46:03 +0900

    ok

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # DevEnvSample
+
+開発環境のサンプル
+
+## 主な機能
+
+- destyle.css
+- sassコンパイル・自動ベンダープレフィックス付与(``.browserslistrc``参照)・ソースマップ付与
+- tsコンパイル(デフォルトでES3)・ソースマップ付与
+- jsバンドル・変換(Babel)
+- ファイル変更検知・自動コンパイル
+- ローカルサーバー起動
+
+## 対象ファイル
+
+変換・監視の対象となるファイル  
+
+### HTML
+
+``src/index.html``
+
+### Sass
+
+エントリーポイント : ``src/style/style.scss``  
+監視対象 : ``["src/style/*scss", "src/style/*/*.scss"]``
+
+### TypeScript
+
+エントリーポイント : ``src/script/main.ts``  
+監視対象 : ``["src/script/*.ts", "src/script/*/*.ts", "src/script/*js", "src/script/*/*.js"]``
+
+### JavaScript
+
+エントリーポイント : ``src/script/main.js``  
+監視対象 : ``["src/script/*.ts", "src/script/*/*.ts", "src/script/*js", "src/script/*/*.js"]``
+
+## npm scripts
+
+main.ts(TypeScript)がエントリーポイントの規定値として設定されているため、main.js(JavaScript)をエントリーポイントとする場合は、npm scriptを書き換える必要がある。
+
+```
+// コンパイル実行
+npm run dev
+```
+
+```
+// ファイル変更検知・自動コンパイル・ローカルサーバー起動
+npm run watch
+```
+
+```
+// ビルド(minify)
+npm run build
+```
+
+npm scriptsは、TypeScriptでの開発を前提として作成。JavaScriptでの開発を行う場合は、``[script]_js``を使用するか、直接npm scriptsを書き換える。  
+エントリーポイントとなるファイルを書き換える場合、および監視対象のファイルを追加/変更する場合は、``gulpfile.js``を編集する。
---
+++ gulpfile.js
@@ -0,0 +1,143 @@
+const gulp = require("gulp");
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require("gulp-sass");
+sass.compiler = require('sass');    // コンパイラにDartSassを指定
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const ts = require("gulp-typescript");
+const minify = require('gulp-minify');
+const webpackStream = require("webpack-stream");
+const webpack = require("webpack");
+const webserver = require("gulp-webserver");
+const fs = require("fs");
+
+// HTMLファイル設置
+gulp.task("_html", done => {
+  fs.copyFile("src/index.html", "dist/index.html", () => {
+    done();
+  });
+})
+
+// sassコンパイル・自動ベンダープレフィックス付与
+gulp.task("_sass", () => {
+  return gulp.src("src/style/style.scss")
+    .pipe(sass({
+      outputStyle: "expanded"
+    }).on("error", sass.logError))
+    .pipe(sourcemaps.write({includeContent: false}))
+		.pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(postcss([
+        autoprefixer({
+          cascade: false,
+          // IEのgridに対応させる
+          grid: true,
+        })
+      ]))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("dist"));
+})
+
+// [build] sassコンパイル・自動ベンダープレフィックス付与
+gulp.task("_sass__build", () => {
+  return gulp.src("src/style/style.scss")
+    .pipe(sass({
+      outputStyle: "compressed"
+    }).on("error", sass.logError))
+    .pipe(postcss([
+        autoprefixer({
+          cascade: false,
+          // IEのgridに対応させる
+          grid: true,
+        })
+      ]))
+    .pipe(gulp.dest("dist"));
+})
+
+// TypeScriptコンパイル
+gulp.task("_ts", () => {
+  return gulp.src("src/script/main.ts")
+    .pipe(sourcemaps.init())
+    .pipe(ts({
+      module: "amd",
+      out: "main.js",
+      // ES3で出力
+      target: "ES3",
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("dist/"));
+})
+
+// [build] TypeScriptコンパイル
+gulp.task("_ts__build", () => {
+  return gulp.src("src/script/main.ts")
+    .pipe(ts({
+      module: "amd",
+      out: "main.js",
+      // ES3で出力
+      target: "ES3",
+      // コメント削除
+      removeComments: true,
+    }))
+    .pipe(minify())
+    .pipe(gulp.dest("dist/"));
+})
+
+// JSをWebpackでバンドル
+gulp.task("_js", () => {
+  const webpackConfig = require('./webpack.development.config.js');
+  return webpackStream(webpackConfig, webpack)
+    .pipe(gulp.dest("dist/"));
+})
+
+// コンパイル・バンドル実行 (TypeScript)
+gulp.task("dev_ts", gulp.parallel("_html", "_sass", "_ts"));
+
+// ビルド
+gulp.task("build_ts", gulp.parallel("_html", "_sass__build", "_ts__build"));
+
+// コンパイル・バンドル実行 (JavaScript)
+gulp.task("dev_js", gulp.parallel("_html", "_sass", "_js"));
+
+// 変更のwatch・ローカルサーバー起動 (TypeScript)
+gulp.task("watch_ts", done => {
+  fs.exists("dist/", result => {
+    if (!result) {
+      console.log("distフォルダが見つかりませんでした。");
+      done();
+    }
+    else
+    {
+      gulp.src("dist")
+        .pipe(webserver({
+          livereload: true,
+          open: true,
+        }))
+
+      gulp.watch("src/index.html", gulp.task("_html"));
+      gulp.watch(["src/style/*scss", "src/style/*/*.scss"], gulp.task("_sass"));
+      gulp.watch(["src/script/*.ts", "src/script/*/*.ts", "src/script/*js", "src/script/*/*.js"], gulp.task("_ts"));
+    }
+  })
+})
+
+// 変更のwatch・ローカルサーバー起動 (JavaScript)
+gulp.task("watch_js", done => {
+  fs.exists("dist/", result => {
+    if (!result) {
+      console.log("distフォルダが見つかりませんでした。");
+      done();
+    }
+    else
+    {
+      gulp.src("dist")
+        .pipe(webserver({
+          livereload: true,
+          open: true,
+        }))
+
+      gulp.watch("src/index.html", gulp.task("_html"));
+      gulp.watch("src/style/*scss", gulp.task("_sass"));
+      gulp.watch(["src/script/*.ts", "src/script/*/*.ts", "src/script/*js", "src/script/*/*.js"], gulp.task("_js"));
+    }
+  })
+})
